69b067a0e46ed11ac4b39e210d699c14be2f58b0
Mats <matsbader@hotmail.com>
2012-05-31 19:35:15 +0200

    Initial solution checkin

---
+++ Recipe/Recipe.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recipe.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public JsonResult Test()
+        {
+            var a = Request;  // analyse for all properties of the call.
+            var p = new Person() { FirstName = "Mats", Name = "Bader" };
+            return Json(p, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string FirstName { get; set; } 
+
+    }
+}
---
+++ Recipe/Recipe.Web.UI/Scripts/MatsLib.js
@@ -0,0 +1,68 @@
+﻿//
+
+var lib = (function () {
+
+    // string constants
+    var deleteButtonClassName = 'deleteButton';
+    var deleteButtonClassSelector = '.' + deleteButtonClassName;
+
+    // the table managed by this library.
+    var $table;
+
+    // private functions
+    function subscribe($trElement) {
+        $trElement.find(deleteButtonClassSelector).click(onDeleteClicked);
+    }
+
+    function onDeleteClicked(event) {
+        var $target = $(event.target);
+        var $row = $target.parent().parent();
+
+        if (!$row || !$row.is('tr')) {
+            throw ('unexpected nesting of elements. delete button is supposed to be in row.');
+        }
+
+        $row.remove();
+    }
+
+
+    return {
+        b: 12,
+        
+        init: function ($tableElement, numberOfInitialRows) {
+
+            if (!$tableElement) {
+                throw ('initialization failure in init(): table element is mandatory argument.');
+            }
+
+            if (!$tableElement.is('table')) {
+                throw ('initialization failure in init(): table element must be of type table.');
+            }
+
+            if (!$tableElement || $tableElement.length != 1) {
+                throw ('initialization failure in init(): only one table element must be passed.');
+            }
+
+            $table = $tableElement;
+            
+            for (var i = 0; i < numberOfInitialRows; i++) {
+                this.appendRow();
+            }
+            
+        },
+
+        appendRow: function () {
+            var $tr = $(document.createElement('tr'));
+
+            var td1 = '<td><input type="text" maxlength="22" size="22"/></td>';
+            var td2 = '<td><textarea rows="2" cols="40" placeholder="test"></textarea></td>';
+            var td3 = '<td><input class="' + deleteButtonClassName + '" type="button" value="-" /></td>';
+
+            $tr.html(td1 + td2 + td3);
+            $table.append($tr);
+            subscribe($tr);
+        },
+
+        d:12
+    };
+})();
